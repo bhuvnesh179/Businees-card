1bfad7b83f2de552c9d2c061691755da27a8effd
bhuvnesh179 <bhuvneshbansal179@gmai.com>
2024-09-15 23:46:38 +0530

    added content in card_component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 
 function App() {
-  
+  return (
+    <>
+    <CardComponent>
+      <h1>Lokeshwar</h1>
+      <p>A TA in the 100Xdevs cohort 2.0</p>
+      <h3>Interests</h3>
+      <p>Ionic</p>
+      <p>Open Source</p>
+      <p>App Dev</p>
+      <button> LinkdedIn</button>
+      <button> Twitter</button>
+      </CardComponent>
+    </>
+  )
+}
+
+function CardComponent({children}){
+  return <div style={{
+    border: "2px solid black", 
+    height:"300px",
+    width:"300px",
+    padding: "10px"
+    }}>
+    {children}
+  </div>
 }
 
 export default App
