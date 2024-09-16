956efa8ee377220327fbfb386b9719a931857775
bhuvnesh179 <bhuvneshbansal179@gmai.com>
2024-09-16 13:42:34 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,18 @@
-
-function App() {
-  return (
-    <>
-    <CardComponent>
-      <h1>Lokeshwar</h1>
-      <p>A TA in the 100Xdevs cohort 2.0</p>
-      <h3>Interests</h3>
-      <p>Ionic</p>
-      <p>Open Source</p>
-      <p>App Dev</p>
-      
-      <button style={{ 
-        backgroundColor: "#04AA6D",
-        border: "none",
-        color: "white",
-        padding: "20px",
-        textAlign: "center",
-        textDecoration: "none",
-        display: "inline-block",
-        fontSize: "16px",
-        margin: "4px 4px",
-        borderRadius: "8px",
-          }}> LinkdedIn</button>
-      <button style={{ 
-          backgroundColor: "#04AA6D",
-          border: "none",
-          color: "white",
-          padding: "20px",
-          textAlign: "center",
-          textDecoration: "none",
-          display: "inline-block",
-          fontSize: "16px",
-          margin: "4px 4px",
-          borderRadius: "8px",  
-          }}> Twitter</button>
-      
-      
-      </CardComponent>
-    </>
-  )
+  import { BusinessCard } from "./components/BusinessCard";
+function App(){
+let Card = {
+"name": "Lokeshwar",
+"description": "A TA in 100Xdevs cohort 2.0",
+"interestList": ["Ionic", "Open Source", "App Dev"],
+  "Linkedin": "https://www.linkedin.com/in/bhuvnesh-bansal-5126371b0/",
+  "Twitter": "https://x.com/hustler_03_06",
 }
+return (
+  <>
+  <BusinessCard innerComponent={Card} />
+    {/* <BusinessCard innerComponent={Card} /> */}
 
-function CardComponent({children}){
-  return <div style={{
-    // border: "2px solid black", 
-    height:"40%",
-    width:"40%",
-    padding: "10px",
-    borderRadius: "8px",
-    boxShadow: "1px 1px 10px 1px grey"
-
-    }}>
-    {children}
-  </div>
+  </>
+)
 }
-
-export default App
+export default App;
---
+++ src/components/BusinessCard.jsx
@@ -0,0 +1,73 @@
+export function BusinessCard(props){
+    
+    return (
+        <div style={style.card}>
+            <h2 style={style.name}>{props.innerComponent.name}</h2>
+            <p style={style.description}>{props.innerComponent.description}</p>
+            <h3 style={style.interestsHeader}>Interest</h3>
+            <ul style={style.interestsList}>
+                {props.innerComponent.interestList.map((interest) => (
+                    <li key={interest} style={style.interestItem}>{interest}</li>
+                ))}
+            </ul>
+
+            <div style={style.socialLinks}>
+                <a href={props.innerComponent.Linkedin} target="_blank" style={{ ...style.link, marginLeft: '0px' }}>Linkedin</a>
+                <a href={props.innerComponent.Twitter} target="_blank" style={{ ...style.link, marginLeft: '0px' }}>Twitter</a>
+
+            </div>
+        </div>
+    )
+}
+
+
+const style = {
+    card: {
+        border: '1px solid #ddd',
+        borderRadius: '8px',
+        padding: '20px',
+        margin: '20px',
+        maxWidth: '400px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        backgroundColor: '#f8f9fa'
+    },
+    name: {
+        fontSize: '24px',
+        marginBottom: '10px',
+        color: '#333',
+    },
+    description: {
+        fontSize: '16px',
+        color: '#555',
+        marginBottom: '15px',
+    },
+    socialLinks: {
+        display: 'flex',
+        marginBottom: '15px',
+    },
+    link: {
+        textDecoration: 'none',
+        color: '#fff', // Text color
+        padding: '10px 15px', // Padding for the button
+        borderRadius: '5px', // Border radius for rounded corners
+        backgroundColor: '#007BFF', // Background color for the button
+        display: 'inline-block', // Display as inline-block to be side by side
+        margin: '10px', // Margin between buttons
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Box shadow for a subtle lift
+    },
+    interestsHeader: {
+        fontSize: '18px',
+        marginBottom: '10px',
+        color: '#333',
+    },
+    interestsList: {
+        listStyle: 'none',
+        padding: 0,
+        margin: 0,
+    },
+    interestItem: {
+        fontSize: '14px',
+        marginBottom: '5px',
+        color: '#555',
+    },
+}
